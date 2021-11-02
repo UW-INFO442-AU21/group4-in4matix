02d9e219abb9477a5afb8538f81ee18c65664db1
ZachGrande <zach.grande23@gmail.com>
2021-11-02 08:42:35 -0700

    Added animated CSS

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,14 @@
+import jackjack from './img/jack-jack.png';
+import 'animate.css';
+
 function HomePage() {
   return(
     <div>
       <h1>
         Mission Statement
       </h1>
+      {/* <p className="animate__animated animate__bounce">Hello</p> */}
+      <img className="animate__animated animate__rotateInDownLeft animate__rotateOutDownRight animate_slower" src={jackjack} alt="logo" />
       <p>
         Imposter Syndrome describes feeling of not being qualified coupled with fear that everyone around you will find out that you are an 'imposter.' Our research suggests that this is common feeliing for many, particularly for female-identifying individuals pursuing a career in tech. Be Incredible seeks to help users explore their relationship with imposter syndrome and how they can  triumph over their related struggles.
       </p>

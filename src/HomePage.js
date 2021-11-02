e0a6983e51c190d89068909e76a3cc16c59445ee
ZachGrande <zach.grande23@gmail.com>
2021-11-02 16:26:33 -0700

    Jack-Jack moves across screen

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,5 @@
 import jackjack from './img/jack-jack.png';
+import './HomePage.css';
 import 'animate.css';
 
 function HomePage() {
@@ -7,8 +8,9 @@ function HomePage() {
       <h1>
         Mission Statement
       </h1>
-      {/* <p className="animate__animated animate__bounce">Hello</p> */}
-      <img className="animate__animated animate__rotateInDownLeft animate__rotateOutDownRight animate_slower" src={jackjack} alt="logo" />
+      {/* <p className="jackjack">Hello</p> */}
+      {/* <img className="animate__animated animate__rotateInDownLeft animate_slower jackjack" src={jackjack} alt="logo" /> */}
+      <img className="jackjack" src={jackjack} alt="logo" />
       <p>
         Imposter Syndrome describes feeling of not being qualified coupled with fear that everyone around you will find out that you are an 'imposter.' Our research suggests that this is common feeliing for many, particularly for female-identifying individuals pursuing a career in tech. Be Incredible seeks to help users explore their relationship with imposter syndrome and how they can  triumph over their related struggles.
       </p>

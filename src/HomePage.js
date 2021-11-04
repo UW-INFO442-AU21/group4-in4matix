3cf3d55d83d05ee0ab84cb9f196728cd84f644a0
rachel-kinkley <rgkinkley@gmail.com>
2021-11-04 10:50:40 -0700

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -12,18 +12,18 @@ function HomePage() {
       {/* <img className="animate__animated animate__rotateInDownLeft animate_slower jackjack" src={jackjack} alt="logo" /> */}
       <img className="jackjack" src={jackjack} alt="logo" />
       <p>
-        Imposter Syndrome describes feelings of inadequacy and self-doubt regardless of experience and education. 
-        Our research suggests that this is a common feeliing for many, particularly for gender 
-        minorities pursuing a career in tech. Be Incredible seeks to help users explore 
+        Imposter Syndrome describes feelings of inadequacy and self-doubt regardless of experience and education.
+        Our research suggests that this is a common feeliing for many, particularly for gender
+        minorities pursuing a career in tech. Be Incredible seeks to help users explore
         their relationship with imposter syndrome and guides them in how they can mitigate the effects.
       </p>
       <h1>
         Quiz Overview
       </h1>
       <p>
-        Psychologists have determined that there are five main archetypes of imposter syndrome. 
-        Be Incredible includes a brief quiz to help you identify which (if any) of the types apply 
-        to you. Take this quiz to learn more about yourself, the experiences of others like you, 
+        Experts have determined that there are five main archetypes of imposter syndrome. 
+        Be Incredible includes a brief quiz to help you identify which (if any) of the types apply
+        to you. Take this quiz to learn more about yourself, the experiences of others like you,
         and what you can do to combat imposter syndrome.
       </p>
       <p class='orange'>

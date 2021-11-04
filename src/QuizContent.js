6da9fa9e98c30846e477eee5a43a5294ced1e788
rachel-kinkley <rgkinkley@gmail.com>
2021-11-04 10:49:29 -0700

    Update QuizContent.js

--- src/QuizContent.js
+++ src/QuizContent.js
@@ -4,7 +4,7 @@ function QuizContent() {
       <p>This is being rendered from QuizContent.js.</p>
       <p>Ideally, we will copy the code from Survey.js into this file. Only code pertaining to building and running the interactive quiz should be stored here.</p>
       <p>Information ABOUT the quiz (i.e. description) should be stored in QuizPage.js</p>
-      <p> Test <p>
+      <p> Test 2<p>
     </div>
   )
 }

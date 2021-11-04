1b6c4a4c8b764a3757aae5573586e97bc3a59b8d
Elise Adams <eliseadams@MacBook-Pro-7.local>
2021-11-04 08:40:06 -0700

    updated homepage mission statement and overview

--- src/HomePage.js
+++ src/HomePage.js
@@ -12,13 +12,19 @@ function HomePage() {
       {/* <img className="animate__animated animate__rotateInDownLeft animate_slower jackjack" src={jackjack} alt="logo" /> */}
       <img className="jackjack" src={jackjack} alt="logo" />
       <p>
-        Imposter Syndrome describes feeling of not being qualified coupled with fear that everyone around you will find out that you are an 'imposter.' Our research suggests that this is common feeliing for many, particularly for female-identifying individuals pursuing a career in tech. Be Incredible seeks to help users explore their relationship with imposter syndrome and how they can  triumph over their related struggles.
+        Imposter Syndrome describes feelings of unqualification and self-doubt regardless of experience and education. 
+        Our research suggests that this is a common feeliing for many, particularly for gender 
+        minorities pursuing a career in tech. Be Incredible seeks to help users explore 
+        their relationship with imposter syndrome and guides them in how they can mitigate the effects.
       </p>
       <h1>
         Quiz Overview
       </h1>
       <p>
-        Psychololigsts have determined that there are five main archetypes of imposter syndrome. Be Incredible includes a breif quiz to help you identify which (if any) of the types apply to you. Take this quiz to learn more about yourself, the experiences of others like you, aand what you can do to combat  imposter syndrome.
+        Psychologists have determined that there are five main archetypes of imposter syndrome. 
+        Be Incredible includes a brief quiz to help you identify which (if any) of the types apply 
+        to you. Take this quiz to learn more about yourself, the experiences of others like you, 
+        and what you can do to combat imposter syndrome.
       </p>
       <p class='orange'>
         Take the quiz here *link to survey*

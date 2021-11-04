7316e85ab31abdd03a530186fd2775cd1376e0d9
theflyingcookie <theflyingcookie1000@gmail.com>
2021-11-04 08:56:50 -0700

    Adding desc titles

--- src/Type-1.js
+++ src/Type-1.js
@@ -1,6 +1,12 @@
 function Type1() {
   return(
-    <h1>This is Imposter Syndrome type 1 - Perfectionist!</h1>
+    <>
+      <h1>This is Imposter Syndrome type 1 - Perfectionist!</h1>
+      <h2>Overview</h2>
+      <h2>General Tips</h2>
+      <h2>Resources</h2>
+      <h2>Quotes</h2>
+    </>
   )
 }
 

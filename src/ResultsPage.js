d6c81631c8692905daa569f4545467762ca74978
ZachGrande <zach.grande23@gmail.com>
2021-11-30 11:07:58 -0800

    Decomposed functions

--- src/ResultsPage.js
+++ src/ResultsPage.js
@@ -6,20 +6,74 @@ import './ResultsPage.css';
 // var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
+function renderChart(results) {
+  const options = {
+    exportEnabled: true,
+    animationEnabled: true,
+    /*title: {
+      display: false,
+      text: "Imposter Syndrome Results"
+    },*/
+    data: [{
+      type: "pie",
+      startAngle: 75,
+      showInLegend: "true",
+      toolTipContent: "{indexLabel} ({name}) {y}%",
+      legendText: "{indexLabel}",
+      indexLabelFontSize: 16,
+      dataPoints: results
+    }]
+  }
+
+  return(
+    <div>
+      <CanvasJSChart options={options} />
+    </div>
+  )
+}
+
+function hasTakenQuiz(results) {
+  for (var i = 0; i < results.length; i++) {
+    if (results[i].y !== 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function contentToRender(results) {
+  if (hasTakenQuiz(results)) {
+    return(
+      <div>
+        <p>Here are your results! We want to remind you that these results are meant to be used as a guideline.</p> {/*Or a similar disclaimer*/}
+        <p>To learn more about each imposter syndrome type, please visit the corresponding tab in the menu bar.</p>
+
+        <div className="flex-container">
+          <div className="flex-item pieChart">{renderChart(results)}</div>
+          <h2 className="flex-item top-result"> top result text will go here</h2> {/* show top result and link to page */}
+          {/* <p>{hasTakenQuiz() ? JSON.stringify(results) : ''}</p> */}
+        </div>
+      </div>
+      );
+  } else {
+    return(
+      <div>
+        <p>The user has not taken the quiz.</p>
+        <Link className="btn btn-danger" role="button" to="/group4-in4matix/quiz">
+                Take the Quiz!
+        </Link>
+      </div>
+    );
+  }
+}
+
 function ResultsPage(props) {
 
   let results = props.results;
 
   console.log("The results are:", results);
 
-  function hasTakenQuiz() {
-    for (var i = 0; i < results.length; i++) {
-      if (results[i].y !== 0) {
-        return true;
-      }
-    }
-    return false;
-  }
+  
 
   // function renderChart() {
   //   var chart = new CanvasJS.Chart("chartContainer", {
@@ -38,31 +92,7 @@ function ResultsPage(props) {
   //   chart.render();
   // }
 
-  function renderChart() {
-    const options = {
-			exportEnabled: true,
-			animationEnabled: true,
-			/*title: {
-        display: false,
-				text: "Imposter Syndrome Results"
-			},*/
-			data: [{
-				type: "pie",
-				startAngle: 75,
-				showInLegend: "true",
-        toolTipContent: "{indexLabel} ({name}) {y}%",
-				legendText: "{indexLabel}",
-				indexLabelFontSize: 16,
-				dataPoints: results
-			}]
-		}
-
-    return(
-      <div>
-        <CanvasJSChart options={options} />
-      </div>
-    )
-  }
+  
 
   //toolTipContent: "<a href='\"'http://apple.com'\"' target='\"'_blank'\"' >{indexLabel} ({name}) </a>",
 
@@ -70,37 +100,13 @@ function ResultsPage(props) {
 
 
   
-  function contentToRender() {
-    if (hasTakenQuiz()) {
-      return(
-        <div>
-          <p>Here are your results! We want to remind you that these results are meant to be used as a guideline.</p> {/*Or a similar disclaimer*/}
-          <p>To learn more about each imposter syndrome type, please visit the corresponding tab in the menu bar.</p>
-
-          <div className="flex-container">
-            <div className="flex-item pieChart">{renderChart()}</div>
-            <h2 className="flex-item top-result"> top result text will go here</h2> {/* show top result and link to page */}
-            {/* <p>{hasTakenQuiz() ? JSON.stringify(results) : ''}</p> */}
-          </div>
-        </div>
-        );
-    } else {
-      return(
-        <div>
-          <p>The user has not taken the quiz.</p>
-          <Link className="btn btn-danger" role="button" to="/group4-in4matix/quiz">
-                  Take the Quiz!
-          </Link>
-        </div>
-      );
-    }
-  }
+  
 
   return(
     <div className="results-page">
       <h1 className="heading-1">Imposter Syndrome Results</h1>
 
-        <div>{contentToRender()}</div> 
+        <div>{contentToRender(results)}</div> 
 
       {/* <p>The user has {hasTakenQuiz() ? 'taken' : 'not taken'} the quiz.</p>
       <p>{hasTakenQuiz() ? JSON.stringify(results) : ''}</p> */}

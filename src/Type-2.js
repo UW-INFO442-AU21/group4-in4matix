e460ef4955914258e5d4314f9c26333256556300
rachel-kinkley <rgkinkley@gmail.com>
2021-11-30 10:51:26 -0800

    video, structure, and quiz color

--- src/Type-2.js
+++ src/Type-2.js
@@ -1,7 +1,35 @@
 function Type2() {
   return(
     <div>
-      <h1>Mr. Incredilbe Imposter Syndrome</h1>
+      <h1>Mr Incredible Imposter Syndrome</h1>
+      <h2>Overview</h2>
+        <p1>
+        </p1>
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/MFd7I4iWeTg?start=94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      <h2>General Tips</h2>
+        <ul>
+          <li>
+          </li>
+          <li>
+          </li>
+          <li>
+          </li>
+        </ul>
+      <h2>Resources</h2>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      <h2>Quotes</h2>
+        <p>
+        </p>
+        <p></p>
     </div>
   )
 }
--- src/Type-4.js
+++ src/Type-4.js
@@ -2,6 +2,35 @@ function Type4() {
   return(
     <div>
       <h1>Dash Imposter Syndrome</h1>
+      <h2>Overview</h2>
+        <p1>
+        </p1>
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/MFd7I4iWeTg?start=139" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      <h2>General Tips</h2>
+        <ul>
+          <li>
+          </li>
+          <li>
+          </li>
+          <li>
+          </li>
+        </ul>
+      <h2>Resources</h2>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      <h2>Quotes</h2>
+        <p>
+       
+        </p>
+        <p></p>
     </div>
   )
 }

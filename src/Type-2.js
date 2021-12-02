c1d253278d24ad4b8c667cff13fd07107f97eeac
ValTse <valeriet2000@gmail.com>
2021-12-01 23:29:43 -0800

    Adding html content
    Adding html content

--- src/Type-2.js
+++ src/Type-2.js
@@ -1,37 +1,54 @@
+import superhero from './img/superhero.svg';
 import './Types.css';
 
 function Type2() {
   return(
     <div>
-      <h1>Mr Incredible - The Superhero</h1>
+      <h1>Mr. Incredible - The Superhero</h1>
+      <img className="page-image" src={superhero} alt="picture of superhero"/>
       <h2>Overview</h2>
         <p1>
+          The superwoman/man represents a person with imposter syndrome that often struggles with work addiction.
+          This person may feel inadequate relative to colleagues and continue to push themselves as hard as possible,
+          regardless of the consequences on mental, physical and emotional health.
         </p1>
         <div className="float-video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/MFd7I4iWeTg?start=94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/MFd7I4iWeTg?start=80" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       <h2>General Tips</h2>
         <ul>
           <li>
+            Impostor workaholics are actually addicted to the validation that comes from working, not to the work
+            itself. Start training yourself to veer away from external validation. No one should have more power
+            to make you feel good about yourself than you–even your boss when they give your project the stamp
+            of approval.
           </li>
           <li>
+            On the flip side, learn to take constructive criticism seriously, not personally.
           </li>
           <li>
+            As you become more attuned to internal validation and able to nurture your inner confidence that states
+            you’re competent and skilled, you’ll be able to ease off the gas as you gauge how much work is reasonable.
           </li>
         </ul>
       <h2>Resources</h2>
         <ul>
           <li>
-            <a href=""></a>
+            <a href="https://fairygodboss.com/articles/5-ways-to-combat-your-need-for-constant-validation-at-work">5 Ways to Combat Your Need For Constant Validation at Work</a>
           </li>
           <li>
-            <a href=""></a>
+            <a href="https://www.livewellwithsharonmartin.com/validate-yourself/">17 Ways to Validate Yourself</a>
           </li>
         </ul>
       <h2>Quotes</h2>
-        <p>
-        </p>
-        <p></p>
+        <blockquote>
+          "I have spent my years since Princeton, while at law school and in my various professional jobs, not 
+          feeling completely a part of the worlds I inhabit. I am always looking over my shoulder wondering if
+          I measure up."
+        </blockquote>
+        <cite>
+          — Sonia Sotomayer, Associate Justice of the Supreme Court of the United States
+        </cite>
     </div>
   )
 }
--- src/Type-3.js
+++ src/Type-3.js
@@ -1,37 +1,56 @@
+import genius from './img/genius.svg';
 import './Types.css';
 
 function Type3() {
   return(
     <div>
       <h1>Edna Mode - The Genius</h1>
+      <img className="page-image" src={genius} alt="picture of genius"/>
       <h2>Overview</h2>
         <p1>
+          “The natural genius represents a person with imposter syndrome 
+          that not only struggles with perfectionism but also sets out 
+          to achieve lofty goals on their first try. These individuals 
+          feel unworthy, guilty and shameful if they cannot easily 
+          complete a task or achieve a goal on their first go. 
         </p1>
         <div className="float-video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/MFd7I4iWeTg?start=131" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/MFd7I4iWeTg?start=80" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       <h2>General Tips</h2>
         <ul>
           <li>
+            To move past this, try seeing yourself as a work in progress. 
+            Accomplishing great things involves lifelong learning and skill
+            building–for everyone, even the most confident people. 
           </li>
           <li>
+            Rather than beating yourself up when you don’t reach your impossibly
+            high standards, identify specific, changeable behaviors that you can improve over time.
           </li>
           <li>
+            For example, if you want to have more impact at the office, 
+            it’s much more productive to focus on honing your presentation 
+            skills than swearing off speaking up in meetings as something you’re “just not good at.” 
           </li>
         </ul>
       <h2>Resources</h2>
         <ul>
           <li>
-            <a href=""></a>
+            <a href="https://www.scotthyoung.com/blog/2009/12/28/the-frustration-barrier-the-key-obstacle-to-being-good-at-anything/">The Frustration Barrier – The Key Obstacle to Being Good at Anything</a>
           </li>
           <li>
-            <a href=""></a>
+            <a href="https://www.forbes.com/sites/forbescoachescouncil/2021/05/26/14-tips-to-help-perfectionists-get-out-of-their-own-way/">14 Tips To Help Perfectionists Get Out Of Their Own Way</a>
           </li>
         </ul>
       <h2>Quotes</h2>
-        <p>
-        </p>
-        <p></p>
+        <blockquote>
+          "I have written 11 books, but each time I think, ‘uh oh, they’re going to
+           find out now. I’ve run a game on everybody, and they’re going to find me out."
+        </blockquote>
+        <cite>
+          — Maya Angelou, Presidential Medal of Freedom Award Winning American Poet, Memoirist, and Civil Rights Activist
+        </cite>
     </div>
   )
 }

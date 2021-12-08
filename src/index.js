c76321437075f8e1a8e54c1643503ee0e965c810
rachel-kinkley <rgkinkley@gmail.com>
2021-12-08 12:31:24 -0800

    halfway done with comments

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 // import { BrowserRouter as Router } from 'react-router-dom';
 import { HashRouter as Router } from 'react-router-dom';
 
+// renders/displays the appropriate page, starting with main. will swtich based on with page user routes to
 ReactDOM.render(
   <React.StrictMode>
     <Router>
@@ -15,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
